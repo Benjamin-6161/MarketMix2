0330fc31af6f45b38167cf4fff184e36e2900163
Benjamin-6161 <egbojambwaec@gmail.com>
2025-08-22 17:54:46 +0100

    Update main_routes.py

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from app.models.business import Business
 from app.models.post import Post
 from app.models.request import Request
 from app.models.user import User
+from flask_login import login_required
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def homepage():
     businesses = Business.query.order_by(Business.created_at.desc()).all()
     posts = Post.query.order_by(Post.created_at.desc()).all()
@@ -14,22 +16,25 @@ def homepage():
     request_users = {}
     for request in requests:
         request_users[request.id] = User.query.get(request.user_id)
-    return render_template('main/homepage.html', businesses=businesses, posts=posts, requests=requests, request_users=request_users)
+    return render_template('main/homepage.html', businesses=businesses, posts=posts, requests=requests, request_users=request_users, app_config=current_app.config)
     
 @main.route('/posts')
+@login_required
 def posts():
     posts = Post.query.order_by(Post.created_at.desc()).all()
-    return render_template('main/posts.html', posts=posts)
+    return render_template('main/posts.html', posts=posts, app_config=current_app.config)
 
 @main.route('/requests')
+@login_required
 def requests():
     requests = Request.query.order_by(Request.created_at.desc()).all()
     request_users = {}
     for request in requests:
         request_users[request.id] = User.query.get(request.user_id)
-    return render_template('main/requests.html', requests=requests, request_users=request_users)
+    return render_template('main/requests.html', requests=requests, request_users=request_users, app_config=current_app.config)
 
 @main.route('/businesses')
+@login_required
 def businesses():
     businesses = Business.query.order_by(Business.created_at.desc()).all()
-    return render_template('main/businesses.html', businesses=businesses)
+    return render_template('main/businesses.html', businesses=businesses, app_config=current_app.config)
